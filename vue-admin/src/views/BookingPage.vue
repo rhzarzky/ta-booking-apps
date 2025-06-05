59a39c862fb8ce007acd3df4affcc599f5b71f95
Rheza Rizky <rhezarizky11@gmail.com>
2025-06-05 17:19:27 +0700

    dashboard list user masih error

--- vue-admin/src/views/BookingPage.vue
+++ vue-admin/src/views/BookingPage.vue
@@ -49,10 +49,9 @@ const totalPages = computed(() => {
 
 // Paginated results from filtered bookings
 const paginatedBookings = computed(() => {
-
+    const sorted = [...filteredBookings.value].sort((a, b) => b.id - a.id);
     const start = (currentPage.value - 1) * itemsPerPage;
     const end = start + itemsPerPage;
-    return filteredBookings.value.slice(start, end);
 });
 
 // Page navigation
