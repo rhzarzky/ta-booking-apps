90c0dad7105321f810a3e3cdf7b57cd2f42bc352
raihan zaki alhafiz <raihanzaki121203@student.uns.ac.id>
2025-07-06 23:24:34 +0700

    update landingpage

--- vue/src/components/Client/Sidebar/SidebarArea.vue
+++ vue/src/components/Client/Sidebar/SidebarArea.vue
@@ -38,7 +38,7 @@ onBeforeUnmount(() => {
 const items = [
   { name: "Dashboard", icon: LayoutDashboard, route: "/client/dashboard" },
   { name: "Service", icon: Users, route: "/client/service" },
-  { name: "History", icon: ActivitySquare, route: "/client/activity" },
+  { name: "History", icon: ActivitySquare, route: "/client/history" },
   { name: "Profile", icon: UserCircle2, route: "/client/profile" },
 ];
 
