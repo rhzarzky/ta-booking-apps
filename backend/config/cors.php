ab424d5bef7f536b07308c1f15c88b1a7627cb8c
Rheza Rizky <rhezarizky11@gmail.com>
2025-07-07 05:39:02 +0700

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://dev-booking.mieso.my.id/', 'https://dev-appointly.mieso.my.id/'],
+    'allowed_origins' => ['*', 'https://dev-booking.mieso.my.id/', 'https://dev-appointly.mieso.my.id/'],
 
     'allowed_origins_patterns' => [],
 
