34c29621459237ea715f2ecbf537891df0586f29
Rheza Rizky <116613764+rhzarzky@users.noreply.github.com>
2025-08-28 13:23:37 +0700

    Update database.php

--- backend/config/database.php
+++ backend/config/database.php
@@ -90,7 +90,7 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            'search_path' => 'laravel',
+            'search_path' => 'public',
             'sslmode' => 'prefer',
         ],
 
